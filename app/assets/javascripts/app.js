be9d7d62ad0bdd40c0fefd3fca76c5cd7fe4858a
orbitka <orbitka@users.noreply.github.com>
2016-06-15 11:25:11 -0400

    looks like it is working

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,19 +7,29 @@ zooApp.controller("AnimalsController", ["$scope", "$http", "$timeout", function(
     console.log("see animals");
   })
 
-  $scope.selectionHandler = function(id, liked){
+//   $scope.selectionHandler = function(id, liked){
+//
+// // ---------version 1: works either liked or disliked------------
+//     $scope.animals.forEach(function(animal) {
+//       if (animal.id == id) {
+//         animal.liked = liked;
+//         $http.put('/api/animals/' + id, { liked: true }).then(console.log('updated-liked'), console.log('error?'));
+//       }
+//       else if (animal.id == id) {
+//         animal.liked = !liked;
+//         $http.put('/api/animals/' + id, { liked: false }).then(console.log('updated-disliked'), console.log('error?'));
+//       }
+//     })
+//     console.log($scope.animals);
+//   }
 
+// version 2: experiment---------
+  $scope.selectionHandler = function(id) {
     $scope.animals.forEach(function(animal) {
       if (animal.id == id) {
-        animal.liked = liked;
-        $http.put('/api/animals/' + id, { liked: true }).then(console.log('updated-liked'), console.log('error?'));
+        animal.liked = !animal.liked;
+        $http.put('/api/animals/' + id, { liked: animal.liked }).then(console.log('updated-disliked'), console.log('error?'));
       }
-      if (animal.id == id) {
-        animal.liked = !liked;
-        $http.put('/api/animals/' + id, { liked: false }).then(console.log('updated-disliked'), console.log('error?'));
-      }
-    })
-    console.log($scope.animals);
+    });
   }
-
 }])
